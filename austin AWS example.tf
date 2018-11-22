ea678581e6445455d6a4ef6b266adf1d991bd665
Austin Lynum <lynumaustin@gmail.com>
2018-11-22 08:55:50 -0600

    Added security group and implimented a script to run on bootup.

--- austin AWS example.tf
+++ austin AWS example.tf
@@ -8,7 +8,24 @@ resource "aws_instance" "webserv1" {
   ami           = "ami-0cf4275f089445589"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
     Name = "austins-webserver"
   }
-}  
+}
+
+resource "aws_security_group" "instance" {
+  name = "austinswebserverinstance"
+
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
